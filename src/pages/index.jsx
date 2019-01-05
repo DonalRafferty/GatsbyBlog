46e44ef146f9787079f6b8ebbd8f80ccd103e4b5
Donal Rafferty <donalrafferty@Donals-MacBook-Pro.local>
2019-01-05 18:15:20 +0000

    Meta data for og and twitter

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,11 +4,13 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Post from '../components/Post'
 import Sidebar from '../components/Sidebar'
+import Banner from '../pages/banner.jpg'
 
 class IndexRoute extends React.Component {
   render() {
     const items = []
-    const { title, subtitle, copyright } = this.props.data.site.siteMetadata
+    const { title, subtitle, copyright, url } = this.props.data.site.siteMetadata
+    const twitterHandle = this.props.data.site.siteMetadata.author.twitter
     const posts = this.props.data.allMarkdownRemark.edges
     posts.forEach(post => {
       items.push(<Post data={post} key={post.node.fields.slug} />)
@@ -19,7 +21,19 @@ class IndexRoute extends React.Component {
         <div>
           <Helmet>
             <title>{title}</title>
+            <meta name="google-site-verification" content="XVCaDE_hoNDn2qS20SdprrhdcUrscutVXd2dvjNvVgg" />
             <meta name="description" content={subtitle} />
+
+            <meta property="og:title" content={title} />
+            <meta property="og:type" content="blog" />
+            <meta property="og:description" content={subtitle}/>
+            <meta property="og:image" content={url + Banner} />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content={twitterHandle} />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={subtitle} />
+            <meta name="twitter:image" content={url + Banner} />
           </Helmet>
           <Sidebar {...this.props} />
           <div className="content">
@@ -43,6 +57,7 @@ export const pageQuery = graphql`
         title
         subtitle
         copyright
+        url
         menu {
           label
           path
--- src/templates/post-template.jsx
+++ src/templates/post-template.jsx
@@ -3,10 +3,12 @@ import Helmet from 'react-helmet'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import PostTemplateDetails from '../components/PostTemplateDetails'
+import Banner from '../pages/banner.jpg'
 
 class PostTemplate extends React.Component {
   render() {
-    const { title, subtitle } = this.props.data.site.siteMetadata
+    const { title, subtitle, url } = this.props.data.site.siteMetadata
+    const twitterHandle = this.props.data.site.siteMetadata.author.twitter
     const post = this.props.data.markdownRemark
     const { title: postTitle, description: postDescription } = post.frontmatter
     const description = postDescription !== null ? postDescription : subtitle
@@ -17,6 +19,17 @@ class PostTemplate extends React.Component {
           <Helmet>
             <title>{`${postTitle} - ${title}`}</title>
             <meta name="description" content={description} />
+
+            <meta property="og:title" content={postTitle} />
+            <meta property="og:type" content="article" />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={url + Banner} />
+
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:creator" content={twitterHandle} />
+            <meta name="twitter:title" content={postTitle} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={url + Banner} />
           </Helmet>
           <PostTemplateDetails {...this.props} />
         </div>
@@ -34,6 +47,7 @@ export const pageQuery = graphql`
         title
         subtitle
         copyright
+        url
         author {
           name
           twitter
